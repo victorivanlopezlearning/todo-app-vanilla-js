2dfe3f48c7b4e44fe027506e104db28f9cba01ba
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-05-29 14:43:43 -0600

    models: todo.model created

---
+++ src/todos/models/todo.model.js
@@ -0,0 +1,14 @@
+
+export class Todo {
+
+    /**
+     * 
+     * @param {String} description Task description 
+     */
+    constructor(description) {
+        this.id = 1;
+        this.description = description;
+        this.done = false;
+        this.createdAt = new Date();
+    }
+}
